3d3107632cc7301fbc1e2cb1e101f6b6d46ca0d9
PCtech Scott <pctechscott@gmail.com>
2016-03-21 00:51:46 -0400

    Added files via upload

--- rock_paper_scissors_lizard_spock_2.rb
+++ rock_paper_scissors_lizard_spock_2.rb
@@ -1,4 +1,4 @@
-# rock_paper_scissors_lizard_spock.rb
+# rock_paper_scissors.rb
 # 02_small_programs
 
 VALID_CHOICES = %w(rock paper scissors lizard Spock)
@@ -9,73 +9,76 @@ def prompt(message)
 end
 
 def win?(first, second)
-    (first == 'rock' && (second == 'scissors' || second == 'lizard')) ||
-    (first == 'paper' && (second == 'rock' || second == 'Spock')) ||
-    (first == 'scissors' && (second == 'paper' || second == 'lizard')) ||
-    (first == 'lizard' && (second == 'Spock' || second == 'paper')) ||
-    (first == 'Spock' && (second == 'scissors' || second == 'rock'))
+  (first == 'rock' && (second == 'scissors' || second == 'lizard')) ||
+  (first == 'paper' && (second == 'rock' || second == 'Spock')) ||
+  (first == 'scissors' && (second == 'paper' || second == 'lizard')) ||
+  (first == 'lizard' && (second == 'Spock' || second == 'paper')) ||
+  (first == 'Spock' && (second == 'scissors' || second == 'rock'))
 end
 
-def display_results(winner)
-  prompt("Winner: #{winner}")
-  prompt("Current scores:")
-#  prompt("Player: #{scores[0]}")
-#  prompt("Computer: #{scores[1]}")
-#  prompt("Ties: #{scores[2]}")
-end
-
-def determine_winner(player, computer)
+def who_won(player, computer)
   if win?(player, computer)
-    winner = 'Player'
+    winner = "Player"
   elsif win?(computer, player)
-    winner = 'Computer'
+    winner = "Computer"
   else
-    winner = 'none'
+    winner = "Nobody"
   end
 end
-
-def increment_score(winner)
-  if winner == 'Player'
-    scores[0] += 1
-  elsif winner == 'Computer'
-    scores[1] += 1
-  else
-    scores[2] += 1
-  end
-end
-
-scores = [0, 0, 0] # player wins, computer wins, ties
 
 loop do
-  choice = ''
-  player_choice = ''
-  winner = ''
-  loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-    prompt("Enter just the first letter. Note that the choice is")
-    prompt("case-sensitive, i.e. 's' for scissors and 'S' for Spock.")
-    choice = Kernel.gets.chomp
 
-    if VALID_INPUTS.include?(choice)
-      player_choice = VALID_CHOICES[VALID_INPUTS.index(choice)]
-      break
-    else
-      prompt("That's not a valid choice.")
+  player_score = 0
+  computer_score = 0
+
+  loop do
+    choice = ''
+    player_choice = ''
+    winner = ''
+
+
+    loop do
+      prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+      prompt("Enter just the first letter. Note that the choice is")
+      prompt("case-sensitive, i.e. 's' for scissors and 'S' for Spock.")
+      choice = Kernel.gets.chomp
+
+      if VALID_INPUTS.include?(choice)
+        player_choice = VALID_CHOICES[VALID_INPUTS.index(choice)]
+        break
+      else
+        prompt("That's not a valid choice.")
+      end
     end
 
-  end
+    computer_choice = VALID_CHOICES.sample
 
-  computer_choice = VALID_CHOICES.sample
+    prompt("You chose: #{player_choice}; Computer chose: #{computer_choice}")
 
-  prompt("You chose: #{player_choice}; Computer chose: #{computer_choice}")
+    winner = who_won(player_choice, computer_choice)
 
-  determine_winner(player_choice, computer_choice)
-  # increment_score(winner)
+    prompt("Winner: #{winner}")
 
-  display_results(winner)
+    if winner == 'Player'
+      player_score += 1
+    elsif winner == 'Computer'
+      computer_score += 1
+    end
+
+    prompt('Current Score:')
+    prompt("Player - #{player_score}    Computer - #{computer_score}")
+
+
+    if player_score == 5 or computer_score == 5
+      prompt("#{winner} wins the match with 5 points!")
+      break
+    end
+end
 
   prompt('Do you want to play again?')
   answer = Kernel.gets.chomp
   break unless answer.downcase.start_with?('y')
 end
+
+
 prompt('Thanks for playing! Goodbye!')
